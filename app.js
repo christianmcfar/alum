5eb70644fbc53093392d2e2ff1fabe2e5c32f97a
christianmcfar <christianmcfa@gmail.com>
2015-12-07 19:02:14 -0500

    Revert "Revert "Revert "Add jquery for offline use"""
    This reverts commit 9b58f94ac214d2019ae9d4e885c1dfaeadfa8914.

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+
+
+var course = {};
+
+var program = {
+	totalCredits : 20,
+	requiredCourses : programArray = [],
+};
+
+
+function addCoruses() {
+	
+}; 
+
+function removeCourses() {
+	
+};
+
+function eligibility(currentCourses) {
+
+};
+
+
+function compare
